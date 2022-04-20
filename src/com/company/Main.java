ea5c381baff88631659f9e0add2c28dabca35327
fslnhv <bgerald30@gmail.com>
2022-04-20 10:01:53 +0300

    Modified program for calculating a falling object.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         double gravity = -9.81; // Earth's gravity in m/s^2
-        double initialVelocity = 0.0;
-        double fallingTime = 10.0;
-        double initialPosition = 0.0;
-        double finalPosition = 0.0;
+        double initialVelocity = 5.0;
+        double fallingTime = 2.0;
+        double initialPosition = 100;
+        double finalPosition;
+
+       finalPosition = 0.5 * (gravity * fallingTime * fallingTime) + (initialVelocity * fallingTime) + initialPosition;
+       finalPosition = (double)(Math.round(finalPosition * 10.0))/10.0;
+
         System.out.println("The object's position after " + fallingTime +
                 " seconds is " + finalPosition + " m.");
     }
